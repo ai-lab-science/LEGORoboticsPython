94d1d33f228e88c8fa9d33a1744edfbcb8713307
Jakob Greten <jakob.greten@student.uni-luebeck.de>
2019-12-11 14:35:16 +0100

    fixed PYTHONPATH

--- Demos/IMU_Demo.py
+++ Demos/IMU_Demo.py
@@ -3,11 +3,17 @@
 from pybricks import ev3brick as brick
 from pybricks.parameters import (Port, Button)
 from pybricks.tools import print, wait
+
+#Fuege den Tools Ordner zum PYTHONPATH hinzu. Nicht notwendig wenn IMU.py im selben Ordner ist
+import sys
+sys.path.append("/home/robot/LEGORoboticsPython/Tools")
+
+#Importiere IMU.py
 from IMU import IMU
 
-#specify Port and mode of Sensor. Possible Modes are TILT, ACCEL, COMPASS, MAG, GYRO
+#Port und Mode des Seonsors festlegen. Moegliche Modes sind TILT, ACCEL, COMPASS, MAG, GYRO
 #TODO: implement ALL mode 
-#more info:http://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/sensor_data.html
+#Info zu Modes:http://docs.ev3dev.org/projects/lego-linux-drivers/en/ev3dev-stretch/sensor_data.html
 imu = IMU(Port.S3, 'GYRO')
 
 while not Button.DOWN in brick.buttons():

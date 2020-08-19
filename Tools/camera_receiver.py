587cc22add804d958117a9811740f0416a5bebd5
Jakob Greten <jakob.greten@student.uni-luebeck.de>
2020-08-19 23:13:14 +0200

    implemented basic interface for pixy2 cameras

---
+++ Tools/camera_receiver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+import os
+
+process = os.popen('python3 /home/robot/LEGORoboticsPython/Tools/camera.py') 
+
+f = open('tmp_camera_data.txt', 'r', os.O_NONBLOCK)
+while not Button.UP in brick.buttons():
+    f.seek(0)
+    text= f.read()
+    
+    data = text.split(',')
+    if data[0]=='' or data[0]=='32896':
+        continue
+    x=int(data[0])
+    y=int(data[1])
+    w=int(data[2])
+    h=int(data[3])
+    #print('running')
+    wait(100)
+    print('x'+str(x))
+    print('y'+str(y))
+
+#f.close()
+process.close()

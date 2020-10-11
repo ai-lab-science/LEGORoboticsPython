4cf5acb96b6f3cc08adfac44f27951dd9a586bb6
Jakob Greten <jakob.greten@gmail.com>
2020-10-11 23:16:31 +0200

    fixed imports

--- Templates/python_intro.py
+++ Templates/python_intro.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks import ev3brick as brick
+from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color,
                                  SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
+ev3 = EV3Brick()
 
 
 
 # Aufgabe 1: Hello World
 # Erzeuge einer Konsolenausgabe(print-Befehl). Es soll "Program starting" ausgegeben werden. 
-# Dieser String(Text) soll erst in einer Variable gespeichert werden und dann ausgegeben werden.
+# Dieser String(Text) soll erst in einer Variable(beliebiger Name) gespeichert werden und dann ausgegeben werden.
+# TODO: Aufgabe bearbeiten
+
+# Aufgabe 2: Endlosschleife
+# Schreibt eine While-Schleife die endlos lange läuft.
+# In der Schleife gebt ihr die Variable counter aus und erhöht den counter jedes mal um eins.
+# Nachdem es funktioniert hat, müsst ihr die While-Schleife mit '#' auskommentieren, 
+# weil sonst der Rest von dem Script nicht ausgeführt wird.
+counter = 0
 # TODO: Aufgabe bearbeiten
 
 
-# Aufgabe 2: While-Schleife
+
+# Aufgabe 3: While-Schleife
 # Jetzt soll ein Countdown von 10 ausgegeben werden. 
 # Dafür sollt ihr eine While-Schleife benutzen und die Variable count ausgeben,
 # die mit 10 initialisiert wird und in jeder Iteration der Schleife verringert werden soll.
-# Baut auch eine Pause in jeder Iteration mit de wait-Befehl ein. 
+# Baut auch eine Pause in jeder Iteration mit dem wait-Befehl ein. 
 # Später könnt ihr diese Wartezeit auskommentieren(mit '#') damit ihr nicht bei jedem Test warten müsst.
 count = 10
 # TODO: Aufgabe bearbeiten
 
 
 
-# Aufgabe 3: EV3 Button
+# Aufgabe 4: EV3 Button
 # Das Programm soll erst gestartet werden wenn der mittlere Button auf dem EV3 gedrückt wird. 
-# Dafür sollt ihr eine While-Schleife und die brick.button() Funktion benutzten.
+# Dafür sollt ihr eine While-Schleife und die ev3.buttons.pressed() Funktion benutzten.
 # Wenn der Button gedrückt wurde soll in der Konsole "Program started" ausgegeben werden
-center_pressed = Brick.Center in brick.button() # Zustand des mittleren Button. Variable muss in Schleife aktualisiert werden
+center_pressed = Button.CENTER in ev3.buttons.pressed() # Zustand des mittleren Button(Boolean). Variable muss in Schleife aktualisiert werden
 # TODO: Aufgabe bearbeiten
-
 
 # Da ein Roboter in der Regel immer wieder die Sensorwerte aktualisieren und evaluieren muss 
 # und die Motoren entsprechend steuern soll, ist es fast immer sinnvoll 
@@ -51,7 +60,7 @@ center_pressed = Brick.Center in brick.button() # Zustand des mittleren Button. 
 #rightMotor = Motor(Port.B)
 
 program_running = True
-while program_running:
+#while program_running:
     # Aufgabe 4: If-Satz und Soundausgabe
     # Wenn der obere Button gedrückt ist soll ein Ton über brick.sound.beep() gespielt werden.
     # TODO: Aufgabe bearbeiten
@@ -62,12 +71,14 @@ while program_running:
     # Dafür benutzt ihr zum Beispiel leftMotor.run(200) für den linken Motor(200 ist die Geschwindigkeit).
     # Falls er nicht gedrückt wurde, soll der Roboter wieder anhalten. Dafür könnt ihr zum Beispiel leftMotor.stop()
     # benutzten. 
+    # TODO: Aufgabe bearbeiten
 
 
     # Aufgabe 6: Ultraschallsensor:
     # Jetzt sollt ihr die Messwerte des Ultraschallsensor auslesen und in der Konsole ausgeben.
     # Dafür braucht ihr nur ultrasonicSensor.distance()
-    
+    # TODO: Aufgabe bearbeiten
+
     
     
     

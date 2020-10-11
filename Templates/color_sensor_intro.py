48df202aada411b2d219cce7bc10f84394d43a47
Jakob Greten <jakob.greten@gmail.com>
2020-10-12 00:37:56 +0200

    fixed imports

--- Templates/color_sensor_intro.py
+++ Templates/color_sensor_intro.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks import ev3brick as brick
+from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import (Port, Stop, Direction, Button, Color,

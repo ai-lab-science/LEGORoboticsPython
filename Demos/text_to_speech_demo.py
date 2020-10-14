162e9f3c71a69ec26b97e72e68422828918b88f6
Jakob Greten <jakob.greten@student.uni-luebeck.de>
2020-10-14 20:23:14 +0200

    added demo for playing simple melodies

--- Demos/text_to_speech_demo.py
+++ Demos/text_to_speech_demo.py
@@ -8,6 +8,19 @@ from pybricks.ev3devices import Motor
 # Objekt der EV3Brick Klasse erstellen
 ev3 = EV3Brick()
 
+ev3.speaker.set_volume(10)
+
+#Spiele "Bruder Jakob" mit der play_notes Funktion
+ev3.speaker.play_notes(['F4/4', 'G4/4', 'A4/4', 'F4/4'])
+ev3.speaker.play_notes(['F4/4', 'G4/4', 'A4/4', 'F4/4'])
+ev3.speaker.play_notes(['A4/4', 'Bb4/4', 'C5/2'])
+ev3.speaker.play_notes(['A4/4', 'Bb4/4', 'C5/2'])
+ev3.speaker.play_notes(['C5/8', 'D5/8', 'C5/8','Bb4/8','A4/4','F4/4'])
+ev3.speaker.play_notes(['C5/8', 'D5/8', 'C5/8','Bb4/8','A4/4','F4/4'])
+ev3.speaker.play_notes(['G4/4', 'C4/4', 'F4/2','G4/4', 'C4/4', 'F4/2'])
+
+
+
 # Die Sprachausgabe des EV3s auf Deutsch stellen. 
 # Für Ausgaben in Englisch kann dieser Schritt weggelassen werden
 ev3.speaker.set_speech_options('de')
@@ -20,8 +33,7 @@ ev3.speaker.set_volume(20)
 
 ev3.speaker.say("Jetzt spreche ich leiser")
 
-
-ev3.speaker.play_notes(['C4/4', 'C4/4', 'G4/4', 'G4/4'])
+ 
 
 
 #ev3.speaker.say("Tennisball gefunden Barcode Nummer 5 Box")
